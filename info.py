448316ca8675b6fda13ef2c72dac3f45ea660cde
rebelitbs <itmtleech@gmail.com>
2025-03-02 10:31:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ MULTI_FSUB = [int(channel_id) for channel_id in environ.get('MULTI_FSUB', '-1002
 # Payment Configuration
 # ============================
 QR_CODE = environ.get('QR_CODE', 'https://t.me/RM_Botz')
-OWNER_UPI_ID = environ.get('Dm for Premium '@VerifiedRebel')
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', '@VerifiedRebel')
 
 # ============================
 # MongoDB Configuration
